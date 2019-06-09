3df65ac8ae0f12acf22adaf20aa7e60b9053db4a
CarolinaPB <carolina.pitabarros@gmail.com>
2019-06-09 19:48:17 +0200

    changes to the x-axis of vertical scaling

--- plot_graphs.R
+++ plot_graphs.R
@@ -8,8 +8,8 @@ legend(8, 300, legend=c("Question 1", "Question 2"),
 
 
 vertical <- read.csv("/Users/Carolina/Documents/GitHub/LDSA_project/csv_performance/LDSA_scaling_times_2_Vertical_scaling.csv", header = TRUE)
-plot(vertical$Threads_num, vertical$Part_1, xlab="# Threads",ylim=c(250,2100), ylab="Time (s)", type="b", col=460, main = "Vertical Scaling")
-axis(1, font = 2)
+plot(vertical$Threads_num, vertical$Part_1, xlab="# Threads",ylim=c(250,2100), ylab="Time (s)", type="b", col=460, main = "Vertical Scaling", xaxt='n')
+axis(1, at = c(1,2,3),font = 2)
 axis(2, font = 2)
 lines(vertical$Threads_num, vertical$Part_2, type="b", pch=0, col=491)
 legend(2.7, 2000, legend=c("Question 1", "Question 2"),
